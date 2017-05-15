505d4bf054e323ce1c5984bb2cce460b211103d2
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-05-15 14:24:37 -0500

    first electron commit

--- src/bgCanvas.js
+++ src/bgCanvas.js
@@ -1,32 +1,36 @@
-function drawBG() {
-  µ('#bgImg').clear();
-  var ctx = µ('#bgImg').ctx;
-  ctx.globalAlpha = .15;
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.moveTo(ctx.canvas.width / 4, -5);
-  ctx.lineTo(0, ctx.canvas.height);
-  ctx.strokeStyle = '#c13828';
-  ctx.stroke();
+obtain(['µ/canvas.js'], function(cnv) {
+  exports.drawBG = function() {
+    µ('#bgImg').clear();
+    var ctx = µ('#bgImg').ctx;
+    ctx.globalAlpha = .15;
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.moveTo(ctx.canvas.width / 4, -5);
+    ctx.lineTo(0, ctx.canvas.height);
+    ctx.strokeStyle = '#c13828';
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.moveTo(ctx.canvas.width / 4, ctx.canvas.height);
-  ctx.lineTo(0, 0);
-  ctx.strokeStyle = '#c13828';
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.moveTo(ctx.canvas.width / 4, ctx.canvas.height);
+    ctx.lineTo(0, 0);
+    ctx.strokeStyle = '#c13828';
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.moveTo(3 * ctx.canvas.width / 4, -5);
-  ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
-  ctx.strokeStyle = '#007aa5';
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.moveTo(3 * ctx.canvas.width / 4, -5);
+    ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
+    ctx.strokeStyle = '#007aa5';
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.lineWidth = 5;
-  ctx.moveTo(3 * ctx.canvas.width / 4, ctx.canvas.height);
-  ctx.lineTo(ctx.canvas.width, 0);
-  ctx.strokeStyle = '#007aa5';
-  ctx.stroke();
-}
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.moveTo(3 * ctx.canvas.width / 4, ctx.canvas.height);
+    ctx.lineTo(ctx.canvas.width, 0);
+    ctx.strokeStyle = '#007aa5';
+    ctx.stroke();
+  };
+
+  provide(exports);
+});
--- src/frameCanvas.js
+++ src/frameCanvas.js
@@ -1,105 +1,110 @@
-var frameStrip = function(){
-	var self = this;
-	var strip = µ("#strip");							//declare the frame strip canvas
-	var temp = document.createElement('canvas','can-vas');		//create a temporary canvas to draw image data to.
+obtain(['µ/canvas.js'], function(cnv) {
+  exports.FrameStrip = function() {
+    console.log('here');
+    var _this = this;
+    var strip = µ('#strip');              //declare the frame strip canvas
+    var temp = document.createElement('canvas', { is: 'can-vas' });    //create a temporary canvas to draw image data to.
 
+    //declare variables to store dimensions.
+    var wid = 0;
+    var hgt = 0;
+    var pb = null;      //the playback object
+    console.log(µ('#strip'));
+    var ctx = strip.ctx;  //the strip context
 
-	//declare variables to store dimensions.
-	var wid = 0;
-	var hgt = 0;
-	var pb = null;			//the playback object
-	var ctx = strip.ctx;	//the strip context
+    //slide is the offset position of the drawing frames; this is used to
+    //create the sliding on or off effect when a frame is added or removed.
+    var slide = 0;
+    var slideInc = 0;
 
-	//slide is the offset position of the drawing frames; this is used to
-	//create the sliding on or off effect when a frame is added or removed.
-	var slide =0;
-	var slideInc =0;
+    //resizes the strip canvas and the temporary (not displayed) canvas.
+    this.resize = function(w, h, imgW, imgH) {
+      wid = w;
+      hgt = h;
+      strip.resize(w, h);
 
-	//resizes the strip canvas and the temporary (not displayed) canvas.
-	this.resize = function(w,h,imgW,imgH){
-		wid=w;
-		hgt=h;
-		strip.resize(w,h);
+      //temp must be sized to the captured frame size, since we are writing imageData to this canvas
+      temp.resize(imgW, imgH);
+    };
 
-		//temp must be sized to the captured frame size, since we are writing imageData to this canvas
-		temp.resize(imgW,imgH);
-	}
+    //bind the playback function to the strip canvas, so we can access pb.fps and pb.book.frame
+    this.registerPB = function(plyBk) {
+      pb = plyBk;
+    };
 
-	//bind the playback function to the strip canvas, so we can access pb.fps and pb.book.frame
-	this.registerPB = function(plyBk){
-		pb=plyBk;
-	}
+    //function used to draw the strip on screen. Only called on resize, frame capturing or deleting, and changing fps
+    this.draw = function() {
+      strip.clear();          //clear the canvas
 
-	//function used to draw the strip on screen. Only called on resize, frame capturing or deleting, and changing fps
-	this.draw = function(){
-		strip.clear();					//clear the canvas
+      //draw a black rectangle over the whole canvas
+      ctx.beginPath();
+      ctx.rect(0, 0, wid, hgt);
+      ctx.fillStyle = '#000000';
+      ctx.fill();
 
-		//draw a black rectangle over the whole canvas
-		ctx.beginPath();
-		ctx.rect(0,0,wid,hgt);
-		ctx.fillStyle="#000000";
-		ctx.fill();
-		//if the playback object has been registered...
-		if(pb){
-			for(var i=0; i<pb.fps+1; i++){ 				//for each frame per second of playback rate
-				var tWid = wid/pb.fps;					//define the width of a cell
-				var tX = wid-(i+1)*wid/pb.fps+slide;	// and its starting position
+      //if the playback object has been registered...
+      if (pb) {
+        for (var i = 0; i < pb.fps + 1; i++) {         //for each frame per second of playback rate
+          var tWid = wid / pb.fps;          //define the width of a cell
+          var tX = wid - (i + 1) * wid / pb.fps + slide;  // and its starting position
 
-				//if there are at least i frames in the "book" object...
-				if(i<pb.book.length()){
-					//write the frame data to the temporary canvas
-					temp.ctx.putImageData(pb.book.frame((pb.book.length()-1)-i),0,0);
-					//and use that canvas to draw the rescaled image into the frame cell.
-					ctx.drawImage(temp,tX+5,5,tWid-10,hgt-10);
+          //if there are at least i frames in the 'book' object...
+          if (i < pb.book.length()) {
+            //write the frame data to the temporary canvas
+            temp.ctx.putImageData(pb.book.frame((pb.book.length() - 1) - i), 0, 0);
 
-					//we do this because images cannot be scaled when using putImageData
-				}
-				else { //if there are more cells than frames in "book"
-					//draw a blue rectangle
-					ctx.beginPath();
-					ctx.rect(tX+5,5,tWid-10,hgt-10);
-					ctx.fillStyle='rgba(0,122,165,.5)';
-					ctx.fill();
-				}
-				//draw a white frame around the frame cell
-				ctx.beginPath();
-				ctx.rect(tX+5,5,tWid-10,hgt-10);
-				ctx.strokeStyle="#ffffff";
-				ctx.lineWidth=2;
-				ctx.stroke();
-				//and write "1 frame" into the middle of the cell, rotating if the text is too long to fit horizontally
-				ctx.fillStyle="#ffffff";
-				ctx.font = "bold 12px sans-serif";
-				ctx.textAlign = "center";
-				ctx.textBaseline = "middle";
-				ctx.save();
-				if(ctx.measureText("1 frame").width>(tWid-10)*.8){
-					ctx.rotate(degToRad(90));
-					ctx.fillText("1 frame",hgt/2,-(tX+5+(tWid-10)/2));
-				}
-				else ctx.fillText("1 frame",tX+5+(tWid-10)/2,hgt/2);
-				ctx.restore();
-			}
-		}
-		if(Math.abs(slide)>=Math.abs(slideInc)){		//if "slide" is greater than 4,
-			slide+=slideInc;			//increment "slide" by slideInc
-			setTimeout(self.draw.bind(self),20);	//and set draw to be called again in 20 milliseconds
-		}
-		else slide = 0; 		//if slide is less than 4, set slide to zero. This needs to be done because after incrementing by slideInc,
-		//slide won't necessarily reach zero; it could be up to 4 off.
-	}
+            //and use that canvas to draw the rescaled image into the frame cell.
+            ctx.drawImage(temp, tX + 5, 5, tWid - 10, hgt - 10);
 
-	this.newFrame = function(){
-		slide = Math.floor(wid/pb.fps);					//set the value of slide to exactly the width of one cell in the frame strip
-		slideInc=-(Math.floor((wid/pb.fps)/5));			//set the increment to -1/5th the initial value of slide.
-		this.draw();									//start drawing the sliding frames.
-	}
+            //we do this because images cannot be scaled when using putImageData
+          } else { //if there are more cells than frames in 'book'
+            //draw a blue rectangle
+            ctx.beginPath();
+            ctx.rect(tX + 5, 5, tWid - 10, hgt - 10);
+            ctx.fillStyle = 'rgba(0,122,165,.5)';
+            ctx.fill();
+          }
 
-	this.popFrame = function(){
-		slide = -Math.floor(wid/pb.fps);				//set the value of slide to exactly the negative width of one cell in the frame strip
-		slideInc=(Math.floor((wid/pb.fps)/5));			//set the increment to 1/5th the initial value of slide.
-		this.draw();									// start drawing the sliding strip
-	}
-};
+          //draw a white frame around the frame cell
+          ctx.beginPath();
+          ctx.rect(tX + 5, 5, tWid - 10, hgt - 10);
+          ctx.strokeStyle = '#ffffff';
+          ctx.lineWidth = 2;
+          ctx.stroke();
 
-var fStrip = new frameStrip();
+          //and write '1 frame' into the middle of the cell, rotating if the text is too long to fit horizontally
+          ctx.fillStyle = '#ffffff';
+          ctx.font = 'bold 12px sans-serif';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.save();
+          if (ctx.measureText('1 frame').width > (tWid - 10) * .8) {
+            ctx.rotate(Math.PI / 2);
+            ctx.fillText('1 frame', hgt / 2, -(tX + 5 + (tWid - 10) / 2));
+          } else ctx.fillText('1 frame', tX + 5 + (tWid - 10) / 2, hgt / 2);
+          ctx.restore();
+        }
+      }
+
+      if (Math.abs(slide) >= Math.abs(slideInc)) {    //if 'slide' is greater than 4,
+        slide += slideInc;      //increment 'slide' by slideInc
+        setTimeout(_this.draw.bind(_this), 20);  //and set draw to be called again in 20 milliseconds
+      } else slide = 0;     //if slide is less than 4, set slide to zero. This needs to be done because after incrementing by slideInc,
+      //slide won't necessarily reach zero; it could be up to 4 off.
+    };
+
+    this.newFrame = function() {
+      slide = Math.floor(wid / pb.fps);          //set the value of slide to exactly the width of one cell in the frame strip
+      slideInc = -(Math.floor((wid / pb.fps) / 5));      //set the increment to -1/5th the initial value of slide.
+      this.draw();                  //start drawing the sliding frames.
+    };
+
+    this.popFrame = function() {
+      slide = -Math.floor(wid / pb.fps);        //set the value of slide to exactly the negative width of one cell in the frame strip
+      slideInc = (Math.floor((wid / pb.fps) / 5));      //set the increment to 1/5th the initial value of slide.
+      this.draw();                  // start drawing the sliding strip
+    };
+  };
+
+  provide(exports);
+});
